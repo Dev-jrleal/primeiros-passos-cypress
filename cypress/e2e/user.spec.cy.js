55bddc1007734d1b767cefa71f81fd2db975ba21
junior-leal <dev.junior.leal@gmail.com>
2024-10-18 05:35:19 -0300

    add page objets

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,15 +1,17 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+import MenuPage from '../pages/menuPage'  
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passowordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: ".oxd-alert",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+   
+    
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
@@ -17,24 +19,29 @@ describe('Orange HRM Tests', () => {
     dateCloseButton: ".--close",
     submitButton: "[type='submit']",
     genercComboBox:".oxd-select-text--after",
+    midName: ".--name-grouped-field",
+    employeeId:':nth-child(1)>:nth-child(1)>.oxd-input-group>:nth-child(2)>.oxd-input',
+    otherId:':nth-child(3)>:nth-child(1)>:nth-child(2)>.oxd-input-group>:nth-child(2)>.oxd-input',
+    drivLicNu:':nth-child(2)>:nth-child(1)>.oxd-input-group>:nth-child(2)>.oxd-input',
+    dataWrapper:':nth-child(2)>.oxd-input-group>:nth-child(2)>.oxd-date-wrapper>.oxd-date-input>.oxd-input',
+
 
   }
 
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passowordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username,userData.userSuccess.password)
+    dashboardPage.checkDashaPage()
+    menuPage.accessMyInfo()
+          
+
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('ola')
+    cy.get(selectorsList.midName).clear().type('ola')
     cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(':nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('EmployeId')
-    cy.get(':nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('OtherId')
-    cy.get(':nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('DrivLicNu')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input').clear().type('2025-03-10')   
+    cy.get(selectorsList.employeeId).clear().type('EmployeId')
+    cy.get(selectorsList.otherId).clear().type('OtherId')
+    cy.get(selectorsList.drivLicNu).clear().type('DrivLicNu')
+    cy.get(selectorsList.dataWrapper).clear().type('2025-03-10')   
     cy.get(selectorsList.dateCloseButton).click()        
     cy.get(selectorsList.submitButton).eq(0).click({force:true})
     cy.get('body').should('contain', 'Successfully Update')
